44cf0890f4f55ffb7dbc518646d9f1fcda37b89e
Shaun Curtis <shaun@coldelm.com>
2023-12-11 20:45:00 +0000

    Update

--- Blazr.RenderLogger.Server/ServerRenderStateService.cs
+++ Blazr.RenderLogger.Server/ServerRenderStateService.cs
@@ -4,22 +4,19 @@ public class ServerRenderStateService : IRenderStateService
 {
     private IHttpContextAccessor _httpContextAccessor;
     private readonly Guid _uid = Guid.NewGuid();
-    private readonly ILogger _logger;
 
     public bool IsPreRender => !(_httpContextAccessor.HttpContext is not null && _httpContextAccessor.HttpContext.Response.HasStarted);
     public string Renderer => "Server";
     public string UidName => _uid.ToString().Substring(0, 8);
     
-    public ServerRenderStateService(IHttpContextAccessor httpContextAccessor, ILogger logger)
+    public ServerRenderStateService(IHttpContextAccessor httpContextAccessor)
     {
         _httpContextAccessor = httpContextAccessor;
-        _logger = logger;
 
         var message = IsPreRender ?
             $"Pre-Render ServerRenderStateService Instance Created with ID: {_uid}"
             : $"ServerRenderStateService Instance Created with ID: {_uid}";
 
         Console.WriteLine(message);
-        _logger.LogDebug(message);
     }
 }
--- Blazr.RenderLogger.WASM/WASMRenderStateService.cs
+++ Blazr.RenderLogger.WASM/WASMRenderStateService.cs
@@ -5,19 +5,15 @@ namespace Blazr.RenderLogger.WASM;
 public class WASMRenderStateService : IRenderStateService
 {
     private readonly Guid _uid = Guid.NewGuid();
-    private readonly ILogger _logger;
     
     public bool IsPreRender => false;
     public string Renderer => "Client";
     public string UidName => _uid.ToString().Substring(0, 8);
 
-    public WASMRenderStateService(ILogger logger )
+    public WASMRenderStateService()
     {
-        _logger = logger;
-
         var message = $"WASMRenderStateService Instance Created with ID: {_uid}";
 
         Console.WriteLine(message);
-        _logger.LogDebug(message);
     }
 }
